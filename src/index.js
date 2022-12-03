1ad99c09328726436ccd1355078ec0365ad75278
Zhakhongir <ismailovzhakhongir@gmail.com>
2022-12-03 20:56:50 +0600

    persist

--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import store, { persistor } from "./store";
 import "./service/fireBase";
 import { HashRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <Provider store={store}>
-    <HashRouter basename="/">
-      <App />
-    </HashRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <HashRouter basename="/">
+        <App />
+      </HashRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,39 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import trelloSlice from "./reducers/trelloSlice";
 import userSlice from "./reducers/logInSlice";
 import modalSlice from "./reducers/modalSlice";
-export const store = configureStore({
-  reducer: {
-    trello: trelloSlice.reducer,
-    user: userSlice.reducer,
-    modal: modalSlice.reducer,
-  },
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const rootReducer = combineReducers({
+  trello: trelloSlice.reducer,
+  user: userSlice.reducer,
+  modal: modalSlice.reducer,
 });
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export const persistor = persistStore(store);
+export default store;
